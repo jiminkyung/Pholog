3900592f338d373a210971d54a9922c7102a746c
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-03-08 16:34:31 +0900

    blog 경로 설정

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def blog_list(request):
+    pass
+
+def blog_create(request):
+    pass
+
+def blog_detail(request):
+    pass
+
+def blog_update(request):
+    pass
+
+def blog_delete(request):
+    pass
