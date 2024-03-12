a1b7fc219246352e86a12cdb51cf8fea1fd23db2
jiminkyung <jimiiwhatttttttttt@gmail.com>
2024-03-12 14:43:02 +0900

    CRUD, 댓글 기능 추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.blog_list, name="blog_list"),
-    path("create/", views.blog_create, name="blog_create"),
-    path("<int:pk>/", views.blog_detail, name="blog_detail"),
-    path("<int:pk>/update/", views.blog_update, name="blog_update"),
-    path("<int:pk>/delete/", views.blog_delete, name="blog_delete")
+    path("", views.post_list, name="post_list"),
+    path("create/", views.post_create, name="post_create"),
+    path("<int:pk>/", views.post_detail, name="post_detail"),
+    path("<int:pk>/update/", views.post_update, name="post_update"),
+    path("<int:pk>/delete/", views.post_delete, name="post_delete"),
+    path("<int:pk>/comment/", views.comment_create, name="comment_create"),
+    path("comment/<int:pk>/update/", views.comment_update, name="comment_update"),
+    path("comment/<int:pk>/delete/", views.comment_delete, name="comment_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,202 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.db.models import Q
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post, Comment, Tag
+from .forms import PostForm, ImageFormSet, CommentForm
 
-def blog_list(request):
-    pass
 
-def blog_create(request):
-    pass
+# 게시글 목록
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'posts/post_list.html'
 
-def blog_detail(request):
-    pass
+    def get_queryset(self):
+        queryset = super().get_queryset()  # 기본 쿼리셋 가져오기
+        q = self.request.GET.get("q")
+        if q:
+            queryset = queryset.filter(
+                Q(title__icontains=q) | Q(content__icontains=q)
+            ).distinct()
+        return queryset
 
-def blog_update(request):
-    pass
 
-def blog_delete(request):
-    pass
+# 게시글 상세페이지
+class PostDetailView(FormMixin, DetailView):
+    model = Post
+    form_class = CommentForm
+    template_name = 'posts/post_detail.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['comment_form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.get_object()
+        comment.author = self.request.user
+        comment.save()
+        return super(PostDetailView, self).form_valid(form)
+
+
+# 태그 처리
+def process_tags(tags_str):
+    tag_names = tags_str.split('#')
+    tag_names = [name.strip() for name in tag_names if name.strip()]
+    tags = []
+    for tag_name in tag_names:
+        tag, created = Tag.objects.get_or_create(name=tag_name)
+        tags.append(tag)
+    return tags
+
+
+# 게시글 생성
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostCreateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['image_formset'] = ImageFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['image_formset'] = ImageFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        image_formset = context['image_formset']
+        if image_formset.is_valid():
+            self.object = form.save(commit=False)
+            self.object.author = self.request.user
+            self.object.save()
+            form.save_m2m()
+        
+            # 태그 처리
+            tags_str = form.cleaned_data.get('tags', '')
+            tags = process_tags(tags_str)
+            self.object.tags.set(tags)
+
+            image_formset.instance = self.object
+            image_formset.save()
+            return redirect('post_list')
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
+# 게시글 수정
+class PostUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/post_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdateView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['image_formset'] = ImageFormSet(self.request.POST, self.request.FILES, instance=self.object)
+        else:
+            context['image_formset'] = ImageFormSet(instance=self.object)
+        if 'image_formset_errors' not in context:  # 오류 메시지를 컨텍스트에 추가하지 않았으면 추가하지 않음
+            context['image_formset_errors'] = None
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        image_formset = context['image_formset']
+        if image_formset.is_valid():
+            self.object = form.save()
+
+            # 태그 처리
+            tags_str = form.cleaned_data.get('tags', '')
+            tags = process_tags(tags_str)
+            self.object.tags.set(tags)
+
+            image_formset.instance = self.object
+            image_formset.save()
+            return redirect('post_list')
+        else:
+            context['image_formset_errors'] = image_formset.errors  # 이미지 폼셋의 오류 메시지를 컨텍스트에 추가
+            return self.render_to_response(self.get_context_data(form=form))
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# 게시글 삭제
+class PostDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+    template_name = 'posts/post_confirm_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+# 댓글 생성
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        form.save()
+        return redirect('post_detail', pk=form.instance.post.pk)
+
+
+# 댓글 수정
+class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'comments/comment_form.html'
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('post_detail', pk=self.object.post.pk)
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+# 댓글 삭제
+class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Comment
+    template_name = 'comments/comment_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
+
+    def test_func(self):
+        comment = self.get_object()
+        return self.request.user == comment.author
+
+
+# URL 경로
+post_list = PostListView.as_view()
+post_detail = PostDetailView.as_view()
+post_create = PostCreateView.as_view()
+post_update = PostUpdateView.as_view()
+post_delete = PostDeleteView.as_view()
+
+comment_create = CommentCreateView.as_view()
+comment_update = CommentUpdateView.as_view()
+comment_delete = CommentDeleteView.as_view()
