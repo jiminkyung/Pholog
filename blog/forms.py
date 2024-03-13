47a05311cd61c390e96d8c7233b6bc40402854ee
jiminkyung <128216954+jiminkyung@users.noreply.github.com>
2024-03-13 21:00:30 +0900

    태그 필드 에러 수정, 카테고리 폼 추가

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import Post, Image, Comment
+from django.core.exceptions import ValidationError
+from .models import Post, Image, Comment, Category
 
 
 class PostForm(forms.ModelForm):
@@ -16,19 +17,24 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ["title", "content",]
-        labels = {"title": "", "content": ""}
+        fields = ["title", "content", "categories"]
         widgets = {
             "title": forms.TextInput(attrs={"class": "input", "placeholder": "제목"}),
             "content": forms.Textarea(
                 attrs={"class": "textarea", "placeholder": "내용을 입력해주세요."}
             ),
+            "categories": forms.SelectMultiple(attrs={"class": "form-select"}),
         }
 
     def __init__(self, *args, **kwargs):
+        user = kwargs.pop("user", None)
         super(PostForm, self).__init__(*args, **kwargs)
+        self.fields["categories"].required = False
         if self.instance.pk:
-            self.fields['tags'].initial = ' '.join(f'#{tag.name}' for tag in self.instance.tags.all())
+            self.fields["tags"].initial = " ".join(f"#{tag.name}" for tag in self.instance.tags.all())
+
+        if user is not None:
+            self.fields["categories"].queryset = Category.objects.filter(author=user)
 
 
 class ImageForm(forms.ModelForm):
@@ -45,9 +51,20 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-        labels = {"content": ""}
         widgets = {
             "content": forms.Textarea(
-                attrs={"class": "textarea", "placeholder": "댓글을 입력해주세요."}
+                attrs={"class": "form-control", "placeholder": "댓글을 입력해주세요.", "rows": 3,}
             ),
         }
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ["name"]
+
+    def clean_name(self):
+        name = self.cleaned_data["name"]
+        if Category.objects.filter(name=name).exists():
+            raise ValidationError(f"'{name}'라는 이름의 카테고리는 이미 존재합니다.")
+        return name
