6fc0a3b1177fa10c42a743c85239756734dec87e
jiminkyung <128216954+jiminkyung@users.noreply.github.com>
2024-03-13 21:01:00 +0900

    카테고리 관리 필드 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Comment, Image, Tag, Category
 
-# Register your models here.
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["author", "message"]
+    search_fields = ["author__username", "message"]
+
+admin.site.register(Post)
+admin.site.register(Image)
+admin.site.register(Comment)
+admin.site.register(Tag)
+admin.site.register(Category)
