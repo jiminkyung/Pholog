3cf42ae41ce3b9b8ee39fc8e0cde7c70bf298de9
jiminkyung <128216954+jiminkyung@users.noreply.github.com>
2024-03-13 21:05:46 +0900

    index 메인 페이지 수정

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-    return render(request, "main/index.html")
+    posts = Post.objects.all().prefetch_related("images")
+    context = {"posts": posts}
+    return render(request, "main/index.html", context)
