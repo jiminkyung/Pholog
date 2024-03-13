a2979c0e4b476312fefb3fa97ab8935ac8388f95
jiminkyung <128216954+jiminkyung@users.noreply.github.com>
2024-03-13 20:56:40 +0900

    카테고리, 태그 url 추가

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.post_list, name="post_list"),
-    path("create/", views.post_create, name="post_create"),
-    path("<int:pk>/", views.post_detail, name="post_detail"),
-    path("<int:pk>/update/", views.post_update, name="post_update"),
-    path("<int:pk>/delete/", views.post_delete, name="post_delete"),
-    path("<int:pk>/comment/", views.comment_create, name="comment_create"),
-    path("comment/<int:pk>/update/", views.comment_update, name="comment_update"),
-    path("comment/<int:pk>/delete/", views.comment_delete, name="comment_delete"),
+    path("signup/", views.signup, name="signup"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("profile/", views.profile, name="profile")
 ]
